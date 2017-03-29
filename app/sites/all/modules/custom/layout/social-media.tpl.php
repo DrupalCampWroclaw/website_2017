d20315a027eb610ad3d30e9434578aeda2732fbb
Daniel <daniel.nowak@droptica.pl>
2017-03-29 14:50:37 +0200

    issue#0: social media

--- app/sites/all/modules/custom/layout/social-media.tpl.php
+++ app/sites/all/modules/custom/layout/social-media.tpl.php
@@ -6,19 +6,7 @@
   </div>
 
   <div id="social-media-fb" class="social-media">
-    <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fdrupalcampwroclawpl&amp;width&amp;height=360&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=476413902469619" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:360px;" allowTransparency="true"></iframe>
+    <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fdrupalcampwroclawpl&amp;width=9000&amp;height=360&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=true&amp;show_border=true&amp;appId=476413902469619" scrolling="yes" frameborder="0" style="border:none; overflow:hidden; height:370px; width: 100%;" allowTransparency="true"></iframe>
   </div>
 
-  <div id="social-media-gplus" class="social-media">
-    <!-- Place this tag where you want the widget to render. -->
-    <div class="g-page" data-href="//plus.google.com/104738857349518335066" data-rel="publisher"></div>
-    <!-- Place this tag after the last widget tag. -->
-    <script type="text/javascript" >
-      (function() {
-        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-        po.src = 'https://apis.google.com/js/platform.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-      })();
-    </script>
-  </div>
 </div>
